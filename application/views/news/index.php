d428cc5c2f5b186b9455a08dd0022997ef46d3a4
mehalu15jan2013@gmail.com <mahi-dani21>
2015-04-28 17:08:56 -0700

    fixes view.php in News

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,3 +1,8 @@
+<?php
+//views/news/index.php
+$this->load->view($this->config->item('theme').'header');
+?>
+
 <h2><?php echo $title ?></h2>
 
 <?php foreach ($news as $news_item): ?>
@@ -6,6 +11,17 @@
         <div class="main">
                 <?php echo $news_item['text'] ?>
         </div>
-        <p><a href="<?php echo 'news/' . $news_item['slug'] ?>">View article</a></p>
+<!--        <p><a href="<?php echo 'news/' . $news_item['slug'] ?>">View article</a></p>-->
+
+<p> <?php
+    
+    echo anchor('news/' . $news_item['slug'],'view Article');
+    
+    ?>
+</p>
 
 <?php endforeach ?>
+<?php
+
+$this->load->view($this->config->item('theme').'footer');
+?>
